faa2a9b11503878ae44caf1e4c4791fea6dce2ba
Christian-nt <cthompson@promoshopinc.com>
2021-07-09 08:43:34 -0600

    updated qty

--- assets/global.js
+++ assets/global.js
@@ -545,11 +545,12 @@ const currentQty = parseInt(document.querySelector('.quantity__input').value);
 const qbtn = document.querySelector('.quantity__button');
 window.addEventListener('click', (event) => {
   if(event.target.name === "plus") {
-    updatedQty = currentQty + 1;
+    currentQty = currentQty + 1;
   }
   if (event.target.name === "minus") {
-    updatedQty = currentQty - 1;
+    currentQty = currentQty - 1;
   } 
+  updatedQty = currentQty
   console.log(updatedQty);
 });
 
